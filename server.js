ece133c8ea14b205ac369755d887c89560824e1e
airasofiaho <hoairait1a@gmail.com>
2017-03-21 14:30:29 +0800

    my first commit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.set('port', (process.env.PORT || 5000));
 //use static files
 app.use(express.static(path.join(__dirname, 'public')));
 //express routes
-app.get('/', function(req, res){
+app.get('/menu', function(req, res){
   res.sendFile(path.join(__dirname, 'menu.html'));
 });
 app.get('/', function(req, res){
